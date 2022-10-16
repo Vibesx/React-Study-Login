be0967750fdc5a24c9b8b37b8a1abfc80293157f
vibesx <lucian.paslariu@gmail.com>
2022-10-16 13:00:11 +0300

    added useEffect logic for user logged in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+	// useEffect always runs once when component is redered the first time
+	// then it runs again every time one of the dependencies changes
+	// as we do not have any dependecies declared, no dependencies can ever change,
+	// so it will only run once, when the component is rendered
+	// In this particular use case we want to make sure that if a user refreshes the page,
+	// he is still logged in (as React has a single-page approach)
+	// So what we do is we store the logged in state somewhere and make sure we get it once when component is ran for the first time
+	useEffect(() => {
+		const storeduserLoggedInInformation =
+			localStorage.getItem("isLoggedIn");
+		if (storeduserLoggedInInformation === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+	const loginHandler = (email, password) => {
+		// We should of course check email and password
+		// But it's just a dummy/ demo anyways
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+	const logoutHandler = () => {
+		localStorage.removeItem("isLoggedIn");
+		setIsLoggedIn(false);
+	};
+
+	return (
+		<React.Fragment>
+			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+			<main>
+				{!isLoggedIn && <Login onLogin={loginHandler} />}
+				{isLoggedIn && <Home onLogout={logoutHandler} />}
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
